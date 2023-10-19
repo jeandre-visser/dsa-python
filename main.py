af99da8d5eb9c02db137d3d2246ffcbb88bbd778
Jeandre Visser <jeandre@nutrimeals.ca>
2023-10-18 21:49:27 -0600

    Linear time loop BIg O

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-nemo = ['nemo']
+import time
+
+
+nemo = ['nemo'] * 100
+
+large = ['nemo'] * 10000
 
 def find_nemo(array):
+    t0 = time.time()
     for i in range(len(array)):
         if array[i] == 'nemo':
             print('Found Nemo!')
-            break
+    t1 = time.time()
+    print(f'Time taken: {t1 - t0} seconds')
         
-find_nemo(nemo) # O(n) --> Linear Time
+find_nemo(large) # O(n) --> Linear Time
