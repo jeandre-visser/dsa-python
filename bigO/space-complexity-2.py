b1c7ba529386d250e9031b5f1b77c86f86d9ed0b
Jeandre Visser <jeandre@nutrimeals.ca>
2023-11-15 20:07:19 -0700

    Space complexity

---
+++ bigO/space-complexity-2.py
@@ -0,0 +1,16 @@
+array = [
+    {
+        'tweet': 'hi',
+        'date': 2012
+    },
+    {
+        'tweet': 'my',
+        'date': 2014
+    },
+    {
+        'tweet': 'teddy',
+        'date': 2018
+    }
+]  # O(n^2)
+print(
+    len('feofemwo'))  # O(1)
